5040cfa46c795d1d065920637c3a2d5e1bb3ed4b
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-02-02 20:05:01 +0530

    migration setup

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,24 @@
-use anyhow::Context;
-use rusqlite::Connection;
+use anyhow::{Context, Result};
+use rusqlite::{params, Connection};
 
-pub fn conn() -> anyhow::Result<Connection> {
-    return Connection::open("donezo.db").context("Error while opening db connection");
+pub fn conn() -> Result<Connection> {
+    Connection::open("donezo.db").context("Failed to open database connection")
+}
+
+pub fn migration(conn: &Connection) -> Result<()> {
+    conn.execute(
+        r#"
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL UNIQUE,
+            done BOOLEAN DEFAULT FALSE,
+            due_time DATETIME,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        );
+        "#,
+        params![],
+    )
+    .context("Failed to run database migration")?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use donezo::command::get_command;
+use donezo::{
+    command::get_command,
+    db::{conn, migration},
+};
 
 fn main() -> anyhow::Result<()> {
+    let conn = conn()?;
+    migration(&conn)?;
+
     let command = get_command();
     println!("{:?}", command.get_matches());
     Ok(())
